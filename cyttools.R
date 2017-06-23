6e15116014152bc84aea89af078e6fbe01d7fe75
Brian Capaldo <bc2zb@d-172-25-229-201.dhcp.virginia.edu>
2017-06-23 15:57:13 -0400

    Troubleshooting algorithm selection

--- cyttools.R
+++ cyttools.R
@@ -8,6 +8,7 @@ Usage:
 cyttools.R [--number=<number>] [--min=<min>] [--max=<max>]
 cyttools.R (-h | --help | --version)
 cyttools.R DIR
+cyttools.R --cluster=<algorithm>
 
 Description:   This program performs automated high parameter cytometry data analysis.
 Options:
@@ -15,6 +16,7 @@ Options:
 --number=<num>  [default: 1] The number of random numbers to generate.
 --min=<min>     [default: 0] The lowest value a random number can have.
 --max=<max>     [default: 1] The highest value a random number can have.
+--cluster=<algorithm>    [default: FlowSOM] The algorithm to use for clustering.
 
 Arguements:
 
@@ -23,6 +25,7 @@ DIR    Provide directory for files to be analyzed.
 
 
 args    <- docopt(doc)
+algList <- c("FlowSOM", "FlowType")
 
 # returns version if version is requested
 if(args$`--version` == T){
@@ -30,6 +33,10 @@ if(args$`--version` == T){
 }else if(is.null(args$DIR)){ # checks for directory provided
   cat("\nERROR: Please provide a directory or file\n")
   
+}else if(args$`--cluster` %in% algList){
+  cat(paste("\nPreparing to run analysis using ", args$`--cluster`, "\n", sep = ""))
+}else if(args$`--cluster` %in% algList == F){
+  cat(paste(c("\nCurrent algorithms for clustering are:", algList), collapse = "\n"), "\n")  
 }else{
   cat(paste("\nRunning analysis on ", args$DIR, "\n", sep = ""))
 }
