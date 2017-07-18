c3ae9e186f7dd57793f099bda2c12a23d790f272
Brian Capaldo <bc2zb@d-172-25-228-114.dhcp.virginia.edu>
2017-07-18 16:39:37 -0400

    Added PANEL req to FlowType call

--- cyttools.R
+++ cyttools.R
@@ -6,7 +6,7 @@ require(methods)
 "
 Usage:
 cyttools.R (-h | --help | --version)
-cyttools.R [--cluster=<algorithm>] DIR
+cyttools.R [--cluster=<algorithm>] DIR PANEL
 cyttools.R --makePanelBlank DIR
 cyttools.R --computeNRS DIR PANEL
 Description:   This program performs automated high parameter cytometry data analysis.
@@ -56,7 +56,7 @@ if(args$`--version` == T){ # returns version if version is requested
   }else if(args$`--cluster` == "FlowType"){
     
     cat(paste("\nPreparing to run analysis using ", args$`--cluster`, " on ", args$DIR, "\n\n", sep = ""))
-    COMMAND <- paste("Rscript Flowtype.R", RESULTS_DIR)
+    COMMAND <- paste("Rscript Flowtype.R", RESULTS_DIR, paste("'", args$PANEL, "'", sep = ""))
     system(command = COMMAND)
   }else{
     cat(paste(c("\nWARNING:","\nAlgorithm not found:", args$`--cluster`), collapse = "\n"), "\n")  
