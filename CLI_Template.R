41452146b4d198966d0247a450f8cffb178b0c76
Brian Capaldo <bc2zb@d-172-25-228-114.dhcp.virginia.edu>
2017-07-18 14:41:14 -0400

    added CLI template

---
+++ CLI_Template.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+require(docopt)
+require(methods)
+
+"
+Usage:
+CLI_Template.R (-h | --help | --version)
+CLI_Template.R DIR
+
+Description:   This script is a template for making docopts compatible Rscripts
+Options:
+--version       Show the current version.
+
+Arguments:
+
+DIR    Provide directory
+" -> doc
+
+
+args <- docopt(doc)
+
+RESULTS_DIR <- args$DIR
+
+cat("\nLoading arguments from", args$DIR, "\n")
+
+load(paste(RESULTS_DIR, "cyttools.args.Rdata", sep = ""))
