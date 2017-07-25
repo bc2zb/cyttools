52702c38b0e3339a062c6f38559a6f4b0a4aaa6d
Brian Capaldo <bc2zb@d-172-25-56-127.dhcp.virginia.edu>
2017-07-25 17:16:47 -0400

    automating cont.matrix TBD

--- compDiffAbndnc.R
+++ compDiffAbndnc.R
@@ -31,88 +31,82 @@ load(paste(RESULTS_DIR, "cyttools.args.Rdata", sep = ""))
 
 library(limma)
 library(tidyverse)
+library(reshape2)
 
 logitTransform <- function(p) { log(p/(1-p)) }
 panelDesign <- read.delim(args$PANEL)
 md <- read.delim(args$METADATA)
 resultsTable <- read.delim(args$FEATURETABLE)
-# 
-# # ALL THIS SHOULD BE DONE BY THE CLUSTERING CALL OR THE CLUSTERING CALL SHOULD PASS ITS DATA ONTO ANOTHER SCRIPT TO DO SO
-# exprTable <- melt(resultsTable,
-#                   measure.vars = colnames(resultsTable)[colnames(resultsTable) %in% panelDesign$name[panelDesign$Ignore == 0] == T],
-#                   id.vars = colnames(resultsTable)[colnames(resultsTable) %in% panelDesign$name == F],
-#                   variable.name = "Metal",
-#                   value.name = "Intensity"
-# )
-# nodeExprTable <- acast(exprTable,
-#                        Mapping + Metal ~ FileNames,
-#                        fun.aggregate = median,
-#                        value.var = "Intensity"
-# )
-# 
-# countTable <- resultsTable[,grep("Mapping|FileNames", colnames(resultsTable))]
-# countTable <- table(countTable$Mapping, countTable$FileNames)
-# 
-# colnames(countTable) <- md$FileName
-# 
-# props_table <- t(t(countTable) / colSums(countTable))
-# 
-# propData <- logitTransform(props_table[,c(1:18)])
-# 
-# targets <- md[c(1:18),]
-# 
-# targets$TimePoint <- factor(targets$TimePoint, levels = c("DBC_4HR", "DBC_24HR"))
-# targets$Condition <- factor(targets$Condition, levels = c("UNSTIM", "10NG", "25NG"))
-# targets$SampleID <- factor(targets$SampleID, levels = unique(targets$SampleID))
-# 
-# exprDesign <- paste(targets$Condition, targets$TimePoint, sep = "_")
-# 
-# design <- model.matrix(~ 0 + exprDesign + targets$SampleID)
-# 
-# colnames(design) <- gsub("exprDesign", "Cnd", colnames(design))
-# colnames(design) <- gsub("targets\\$SampleID", "", colnames(design))
-# 
-# fit <- lmFit(propData, design = design)
-# cont.matrix <- makeContrasts(Low_4HR = Cnd10NG_DBC_4HR - CndUNSTIM_DBC_4HR,
-#                              High_4HR = Cnd25NG_DBC_4HR - CndUNSTIM_DBC_4HR,
-#                              Low_24HR = Cnd10NG_DBC_24HR - CndUNSTIM_DBC_24HR,
-#                              High_24HR = Cnd25NG_DBC_24HR - CndUNSTIM_DBC_24HR,
-#                              levels = design)
-# 
-# fit2 <- contrasts.fit(fit, cont.matrix)
-# fit2 <- eBayes(fit2)
-# 
-# diffAbndncStatsTable <- data.frame()
-# 
-# for ( i in 1:length(colnames(fit2))){
-#   nextPart <- topTable(fit2, coef = i, number = Inf)
-#   nextPart$Source <- rep(colnames(fit2)[i], nrow(nextPart))
-#   nextPart$Mapping <- row.names(nextPart)
-#   diffAbndncStatsTable <- rbind(diffAbndncStatsTable, nextPart)
-# }
-# 
-# fit <- lmFit(nodeExprTable[,c(1:18)], design = design)
-# cont.matrix <- makeContrasts(Low_4HR = Cnd10NG_DBC_4HR - CndUNSTIM_DBC_4HR,
-#                              High_4HR = Cnd25NG_DBC_4HR - CndUNSTIM_DBC_4HR,
-#                              Low_24HR = Cnd10NG_DBC_24HR - CndUNSTIM_DBC_24HR,
-#                              High_24HR = Cnd25NG_DBC_24HR - CndUNSTIM_DBC_24HR,
-#                              levels = design)
-# 
-# fit2 <- contrasts.fit(fit, cont.matrix)
-# fit2 <- eBayes(fit2)
-# 
-# diffExprStatsTable <- data.frame()
-# 
-# for ( i in 1:length(colnames(fit2))){
-#   nextPart <- topTable(fit2, coef = i, number = Inf)
-#   nextPart$Source <- rep(colnames(fit2)[i], nrow(nextPart))
-#   nextPart$RowNames <- row.names(nextPart)
-#   nextPart <- separate(nextPart,
-#                        RowNames,
-#                        c("Mapping", "Metal"),
-#                        "_")
-#   diffExprStatsTable <- rbind(diffExprStatsTable, nextPart)
-# }
+
+# ALL THIS SHOULD BE DONE BY THE CLUSTERING CALL OR THE CLUSTERING CALL SHOULD PASS ITS DATA ONTO ANOTHER SCRIPT TO DO SO
+exprTable <- melt(resultsTable,
+                  measure.vars = colnames(resultsTable)[colnames(resultsTable) %in% panelDesign$name[panelDesign$Ignore == 0] == T],
+                  id.vars = colnames(resultsTable)[colnames(resultsTable) %in% panelDesign$name == F],
+                  variable.name = "Metal",
+                  value.name = "Intensity"
+)
+nodeExprTable <- acast(exprTable,
+                       Mapping + Metal ~ FileNames,
+                       fun.aggregate = median,
+                       value.var = "Intensity"
+)
+
+countTable <- resultsTable[,grep("Mapping|FileNames", colnames(resultsTable))]
+countTable <- table(countTable$Mapping, countTable$FileNames)
+
+colnames(countTable) <- md$FileName
+
+props_table <- t(t(countTable) / colSums(countTable))
+
+propData <- logitTransform(props_table)
+
+targets <- md
+
+targets$TimePoint <- factor(targets$TimePoint, levels = unique(targets$TimePoint))
+targets$Condition <- factor(targets$Condition, levels = unique(targets$Condition))
+targets$SampleID <- factor(targets$SampleID, levels = unique(targets$SampleID))
+
+exprDesign <- paste(targets$Condition, targets$TimePoint, sep = "_")
+
+design <- model.matrix(~ 0 + exprDesign + targets$SampleID + targets$Group)
+
+colnames(design) <- gsub("exprDesign", "Cnd", colnames(design))
+colnames(design) <- gsub("targets\\$SampleID|targets\\$Group", "BatchEffect", colnames(design))
+
+fit <- lmFit(propData, design = design)
+cont.matrix <- makeContrasts(PBMC = CndLJI_Colon - CndVA_Colon,
+                             Colon = CndLJI_PBMC - CndVA_PBMC,
+                             levels = design)
+
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2)
+
+diffAbndncStatsTable <- data.frame()
+
+for ( i in 1:length(colnames(fit2))){
+  nextPart <- topTable(fit2, coef = i, number = Inf)
+  nextPart$Source <- rep(colnames(fit2)[i], nrow(nextPart))
+  nextPart$Mapping <- row.names(nextPart)
+  diffAbndncStatsTable <- rbind(diffAbndncStatsTable, nextPart)
+}
+
+fit <- lmFit(nodeExprTable, design = design)
+
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2)
+
+diffExprStatsTable <- data.frame()
+
+for ( i in 1:length(colnames(fit2))){
+  nextPart <- topTable(fit2, coef = i, number = Inf)
+  nextPart$Source <- rep(colnames(fit2)[i], nrow(nextPart))
+  nextPart$RowNames <- row.names(nextPart)
+  nextPart <- separate(nextPart,
+                       RowNames,
+                       c("Mapping", "Metal"),
+                       "_")
+  diffExprStatsTable <- rbind(diffExprStatsTable, nextPart)
+}
 
 
 workspaceFile <- paste(RESULTS_DIR, "compDiffAbndncWorkspace.Rdata", sep = "")
