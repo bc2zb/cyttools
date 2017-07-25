7542463a66852fee88096fb9fcef16f5dd4b684d
Brian Capaldo <bc2zb@d-172-25-56-127.dhcp.virginia.edu>
2017-07-25 14:39:34 -0400

    Added CLI to makeMetaDataBlank.R

--- makeMetaDataBlank.R
+++ makeMetaDataBlank.R
@@ -1,3 +1,32 @@
+#!/usr/bin/env Rscript
+
+require(docopt)
+require(methods)
+
+"
+Usage:
+makeMetaDataBlank.R (-h | --help | --version)
+makeMetaDataBlank.R DIR
+
+Description:   This script is a template for making docopts compatible Rscripts
+Options:
+--version       Show the current version.
+
+Arguments:
+
+DIR    Provide directory
+" -> doc
+
+
+args <- docopt(doc)
+
+RESULTS_DIR <- args$DIR
+
+cat("\nLoading arguments from", args$DIR, "\n")
+
+load(paste(RESULTS_DIR, "cyttools.args.Rdata", sep = ""))
+
+
 # USER SHOULD BE PASSING THIS IN AS A SEPARATE FILE
 md <- data.frame(FileName = colnames(countTable))
 md$FileName <- gsub("/Volumes/som-fccf/FC_McNamaraCA/McSkimming/170602 Capaldo HK exp stim cells fcs files/Myeloid export//", "", md$FileName)
@@ -8,3 +37,7 @@ md <- separate(md,
                remove = F
 )
 md$SampleID <- gsub("\\.fcs", "", md$SampleID)
+
+workspaceFile <- paste(RESULTS_DIR, "makeMetaDataBlankWorkspace.Rdata", sep = "")
+
+save.image(file = workspaceFile)
