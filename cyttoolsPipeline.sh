6f349823f8d5779e60457ab861e23da478a9b7b0
Brian Capaldo <bc2zb@d-172-25-56-127.dhcp.virginia.edu>
2017-07-28 12:31:52 -0400

    updated cyttoolsPipeline.sh to be compatible with v0.2

--- cyttoolsPipeline.sh
+++ cyttoolsPipeline.sh
@@ -14,35 +14,21 @@ cd $CYTTOOLS_LOCATION
 
 # if needed, generate panel and meta data files. DO THIS BEFORE YOU RUN THIS SCRIPT!
 
-# Rscript cyttools.R --makePanelBlank "$FCS_DIR"
-# Rscript cyttools.R --makeMetaDataBlank "$FCS_DIR"
-
-# mkdir "$RESULTS_BLANKS"
-# find cyttoolsResults/* -type f -print | xargs -I {} mv {} "$RESULTS_BLANKS"
+# Rscript cyttools.R --makePanelBlank "$FCS_DIR" "$RESULTS_BLANKS"
+# Rscript cyttools.R --makeMetaDataBlank "$FCS_DIR" "$RESULTS_BLANKS"
 
 # perfrom clustering analysis, WARNING FlowType takes a long time to run and will eat up most of your memory
 
-Rscript cyttools.R --cluster=FlowSOM "$FCS_DIR" "$PANEL"
-
-# move clustering results directory from cyttoolsResults to your experiment location, this script will automatically create the directory, you should not make it beforehand
-
-mkdir "$RESULTS_CLUSTERING"
-find cyttoolsResults/* -type f -print | xargs -I {} mv {} "$RESULTS_CLUSTERING"
+Rscript cyttools.R --cluster=FlowSOM "$FCS_DIR" "$PANEL" "$RESULTS_CLUSTERING"
 
 # perform differential analysis, Rscript cyttools.R --cluster will automatically generate feature tables to be used in this command, and the above BASH command moves them to where the next command can find them
 
 ABNDNC_FILE_NAME="nodeAbundanceFeatureTable.txt"
 ABNDNC_FEATURE_TABLE="$RESULTS_CLUSTERING/$ABNDNC_FILE_NAME"
 
-Rscript cyttools.R --compDiffAbndnc  "$PANEL" "$ABNDNC_FEATURE_TABLE" "$METADATA"
+Rscript cyttools.R --compDiffAbndnc  "$PANEL" "$ABNDNC_FEATURE_TABLE" "$METADATA" "$RESULTS_DIFFERENTIAL"
 
 EXPR_FILE_NAME="nodeExpressionFeatureTable.txt"
 EXPR_FEATURE_TABLE="$RESULTS_CLUSTERING/$EXPR_FILE_NAME"
 
-Rscript cyttools.R --compDiffExpr "$PANEL" "$EXPR_FEATURE_TABLE" "$METADATA"
-
-# move differential analysis results directory from cyttoolsResults to your experiment location
-
-mkdir $RESULTS_DIFFERENTIAL
-find cyttoolsResults/* -type f -print | xargs -I {} mv {} $RESULTS_DIFFERENTIAL
-
+Rscript cyttools.R --compDiffExpr "$PANEL" "$EXPR_FEATURE_TABLE" "$METADATA" "$RESULTS_DIFFERENTIAL"
